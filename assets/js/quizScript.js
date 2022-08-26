e23a6a5d8c8174c91483aafbe70d448f37820c07
Brian Campbell <brian.campbell003@gmail.com>
2022-08-25 21:29:11 -0500

    Quiz split to new js, response from zip into api call rendered to page
    Co-authored-by: Austin McIntyre <aumcintyre@gmail.com>

---
+++ assets/js/quizScript.js
@@ -0,0 +1,80 @@
+// SCRIPT FOR CAFFEINE INTAKE
+var start = document.querySelector("#start-button")
+start.addEventListener("click", nextQuestion);
+
+let quizMain = $('#caffeineQuizMain');
+let questionEl = $('#question');
+let questionIndex = 0;
+let caffeineScore = 0;
+
+function nextQuestion() {
+  const questionArr = [
+    "How many cans of cola (Coke, Pepsi, etc.- diet or otherwise) have you had today?",
+    "How many shots of espresso have you had today, including those in lattes, cappucinos, and the like?",
+    "How many cups of regular coffee have you had today?",
+    "How many energy drinks have you had today?",
+    "If there's any other caffeine you've taken in today, then please estimate the amount in milligrams (mg):"
+  ]
+  
+  const answersArr = [
+    ["Zero", "One", "Two", "Three or more"],
+    ["Zero", "One", "Two", "Three or more"],
+    ["Zero", "One", "Two", "Three or more"],
+    ["Zero", "One", "Two", "Three or more"],
+    ["None", "50mg", "100mg", "200mg or more"]
+  ]
+
+    quizMain.removeClass( "hide" );
+    questionEl.text(questionArr[questionIndex]);
+
+
+    for (let i = 0; i < questionArr.length; i++) {
+        let answersEl = $('#answers');
+        answersEl.children().children().eq(i).children().text( answersArr[questionIndex][i] );
+    }
+
+    var answer1 = document.querySelector("#answer1");
+    var answer2 = document.querySelector("#answer2");
+    var answer3 = document.querySelector("#answer3");
+    var answer4 = document.querySelector("#answer4");
+
+    answer1.addEventListener("click", saveAnswer);
+    answer2.addEventListener("click", saveAnswer);
+    answer3.addEventListener("click", saveAnswer);
+    answer4.addEventListener("click", saveAnswer);
+}
+
+function saveAnswer(event) {
+    console.log(event.target.value);
+    let answerVal = event.target.value ;
+
+    if (answerVal === "b") {
+      caffeineScore++
+    } else if (answerVal === "c") {
+      caffeineScore +=2
+    } else if (answerVal === "d") {
+      caffeineScore +=3
+    }
+
+    questionIndex++;
+    if (questionIndex < 5) {
+    nextQuestion();
+    } else {
+    quizOver(caffeineScore);
+    }
+}
+
+function quizOver(caffeineScore) {
+  console.log(caffeineScore);
+  quizMain.addClass( "hide" );
+  $('#caffeineQuizStart').children(1).eq(1).addClass( "hide" );
+
+  let quizResultsEl = $('#caffeineQuizOver');
+  if (caffeineScore < 2) {
+    quizResultsEl.text ("Doctor, doctor, get us a coffee, stat! Return to our main page to find coffee recipes or local cafes!")
+  } else if ( (2 <= caffeineScore) & (caffeineScore <= 5) ) {
+    quizResultsEl.text ("You've had plenty of caffeine today, but if you insist then by all means return to our main page for drink recipts or local cafes!")
+  } else {
+    quizResultsEl.text ("Woah!!! Slow down partner. Maybe some water instead of a coffee?")
+  }
+}
